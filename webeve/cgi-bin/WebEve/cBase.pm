b3113cb563d027be93ec59fce6080bfbf013e732
Christopher Hofmann <cwh42@users.noreply.github.com>
2003-03-31 17:01:03 +0000

    new base class with interface to Config.pm and the methos "logger"

---
+++ webeve/cgi-bin/WebEve/cBase.pm
@@ -0,0 +1,57 @@
+# $Id$
+# ===============================================================================
+# The Base-Class 'WebEve'
+# ===============================================================================
+
+package WebEve::cBase;
+
+use strict;
+use Date::Calc qw( Today_and_Now );
+use WebEve::Config;
+
+sub getConfig
+{
+    my $self = shift;
+
+    my $Param = shift;
+
+    if( $Param eq 'BaseURL' )
+    {
+	return $BaseURL;
+    }
+    elsif( $Param eq 'BasePath' )
+    {
+	return $BasePath;
+    }
+    elsif( $Param eq 'TemplatePath' )
+    {
+	return $TemplatePath;
+    }
+    elsif( $Param eq 'LogFile' )
+    {
+	return $LogFile;
+    }
+    elsif( $Param eq 'LogPath' )
+    {
+	return $LogPath;
+    }
+    elsif( $Param eq 'DB' )
+    {
+	return \%DB;
+    }
+}
+
+sub logger($)
+{
+    my $self = shift;
+    my ($message) = @_;
+    my $UserName = $self->{'UserName'} || $self->{'REMOTE_HOST'};
+
+    open( LOG, ">>". $self->{'Logfile'} );
+
+    printf( LOG "%4d-%02d-%02d %02d:%02d:%02d %s: %s\n", Today_and_Now(), $UserName, $message );
+
+    close( LOG );
+}
+
+1;
