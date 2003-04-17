4798a45b79319c47e8ad365dc85a2cba50fc173e
cwh <cwh@53118c76-b048-11df-9244-49f539f68fc7>
2003-04-17 07:57:11 +0000

    Backup of the old obsolete WebEve-scripts

---
+++ trunk/webeve/cgi-bin/old/ClassTest.pl
@@ -0,0 +1,53 @@
+#!/usr/bin/perl -w
+
+# $Id$
+
+use WebEve::cDate;
+use WebEve::cEvent;
+use WebEve::cEventList;
+
+$o = WebEve::cDate->new( '12' );
+
+print "STR:".$o->getDateStr."\n";
+print "OVR:".$o->isOver."\n";
+print "TOD:".$o->isToday."\n";
+print "NWK:".$o->isNextWeek."\n";
+print "DAY:".$o->getDay."\n";
+print "MON:".$o->getMonth."\n";
+print "YEA:".$o->getYear."\n";
+print "DAT:".join('-', $o->getDate)."\n";
+
+
+$Event = WebEve::cEvent->newFromDB( 310 );
+
+print "Date:".$Event->getDate->getDateStr."\n";
+print "ValidDate:".$Event->getDate->isValid."\n";
+print "Time:".$Event->getTime."\n";
+print "Place:".$Event->getPlace."\n";
+print "Desc:".$Event->getDesc."\n";
+print "isPublic:".$Event->isPublic."\n";
+
+print "Org:".$Event->getOrg."\n";
+print $Event->setOrgID(14)."\n";
+print "Org:".$Event->getOrg."\n";
+
+print "isPublic:".$Event->isPublic."\n";
+print $Event->setIsPublic(0)."\n";
+print "isPublic:".$Event->isPublic."\n";
+
+
+$EventList = WebEve::cEventList->new( PublicOnly => 1, PerPage => 20, Page => 3 );
+print $EventList->readData();
+print "\n";
+
+foreach my $Ev ( $EventList->getDateList() )
+{
+    print " ID:".$Ev->getID."\n";
+    print " Date:".$Ev->getDate->getDateStr."\n";
+    print " ValidDate:".$Ev->getDate->isValid."\n";
+    print " Time:".$Ev->getTime."\n";
+    print " Place:".$Ev->getPlace."\n";
+    print " Desc:".$Ev->getDesc."\n";
+    print " isPublic:".$Ev->isPublic."\n";
+    print "---------------------------------------\n";
+}
