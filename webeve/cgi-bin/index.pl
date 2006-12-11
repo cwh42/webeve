e7906811ec375baf86f4b3ee52ebfdcb7212dfb7
Christopher Hofmann <cwh42@users.noreply.github.com>
2006-12-11 11:30:16 +0000

    - Created index.pl with information about webeve - Moved parts of kalender.pl to View.pm

---
+++ webeve/cgi-bin/index.pl
@@ -0,0 +1,90 @@
+#!/usr/bin/perl -w
+
+use strict;
+use WebEve::Config;
+use WebEve::View;
+use CGI;
+use HTML::Template;
+use Data::Dumper;
+use File::Basename;
+
+main();
+
+sub main
+{
+    my $include_path = "../include/";
+
+    my $cgi = CGI->new();
+    my $tmpl = HTML::Template->new( filename => "$TemplatePath/index.tmpl" );
+
+    my @Menu = ( { Title => 'Startseite', RunMode => 'start' },
+		 { Title => 'Über Webeve', RunMode => 'about' },
+		 { Title => 'Termine verwalten', RunMode => 'manage' },
+		 { Title => 'Impressum & Kontakt', RunMode => 'contact' } );
+
+    my $mode = $cgi->param('mode');
+    $mode = 'start' unless( scalar( grep { $_->{RunMode} eq $mode } @Menu ) );
+
+    my $content = '';
+
+    if( $mode eq 'start' )
+    {
+	$content = CalendarHTML(basename($0));
+    }
+    else
+    {
+	open( IN, "<$include_path/$mode.inc");
+	while( my $ln = <IN> )
+	{
+	    $content .= $ln;
+	}
+	close( IN );
+    }
+    $tmpl->param( content => $content );
+
+    my $menu = NavMenuCleanup( \@Menu);
+    $tmpl->param( menu => $menu );
+
+    print $cgi->header( -charset => 'UTF-8' );
+    print $tmpl->output();
+}
+
+sub NavMenuCleanup
+{
+    my $Entries = shift;
+    my @Result = ();
+
+    my $FileName = basename( $0 );
+    my $rm = '';
+
+    foreach my $Entry (@$Entries)
+    {
+        my $Admin = delete( $Entry->{'Admin'} );
+        my $RunMode = delete( $Entry->{'RunMode'} );
+
+        if( 1 )
+        {
+            if( exists( $Entry->{'SubLevel'} ) && $Entry->{'SubLevel'} )
+            {
+                my $tmp = NavMenuCleanup( $Entry->{'SubLevel'} );
+                $Entry->{'SubLevel'} = $tmp;
+            }
+
+            if( $RunMode && $RunMode eq $rm )
+            {
+                $Entry->{'Current'} = 1;
+            }
+            else
+            {
+                $Entry->{'Current'} = 0;
+            }
+
+            $Entry->{'FileName'} = "$FileName?mode=$RunMode" unless( $Entry->{'FileName'} ) ;
+
+            push( @Result, $Entry );
+        }
+    }
+
+    return \@Result;
+}
+
