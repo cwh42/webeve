69c953132892e845d1577f5f38d402fed4bae24b
cwh <cwh@53118c76-b048-11df-9244-49f539f68fc7>
2006-12-11 11:30:16 +0000

    - Created index.pl with information about webeve - Moved parts of kalender.pl to View.pm

---
+++ trunk/webeve/cgi-bin/WebEve/View.pm
@@ -0,0 +1,154 @@
+package WebEve::View;
+
+use strict;
+use WebEve::Config;
+use WebEve::cEventList;
+use HTML::Template;
+use Exporter;
+use vars qw(@ISA @EXPORT);
+
+@ISA    = qw(Exporter);
+@EXPORT = qw( CalendarHTML ); 
+
+sub CalendarHTML
+{
+    my $Url = shift || '';
+    my $SubTmplName = "kalender-table.tmpl";
+
+    my $query = new CGI;
+
+    my $Organization = $query->param('Verein') || $query->param('Org') || 0; 
+    my $Intern = $query->param('Intern') || 0; 
+    $Intern = 0 if ! $Organization; 
+
+    my $Page = $query->param('Seite') || '1'; 
+
+    my $tmp = sprintf("custom/template-%02d-advanced.tmpl", $Organization);
+    $SubTmplName = $tmp if( -f "$TemplatePath/$tmp" );
+
+    my $SubTmpl = HTML::Template->new(filename => "$TemplatePath/$SubTmplName",
+				      die_on_bad_params => 0,
+				      loop_context_vars => 1);
+
+    # -----------------------------------------------------------------------
+
+    my $EventList = WebEve::cEventList->new( 'PerPage' => 15,
+					     'Page' => $Page,
+					     'PublicOnly' => !$Intern,
+					     'ForOrgID' => $Intern ? $Organization : 0 );
+    $EventList->readData();
+
+    $SubTmpl->param( PageSwitch( Org => $Organization,
+				 ScriptURL => $Url,
+				 Page => $Page,
+				 Pages => $EventList->getPageCount(),
+				 Internal => $Intern ) );
+    
+    # -----------------------------------------------------------------------
+
+    my $Today = WebEve::cDate->new('today');
+
+    # Terminliste
+    # -----------------------------------------------------------------------
+    my $LastDate = '';
+    my $LastMonth = 0;
+    my $LastYear = 0;
+    my $LastWasNextWeek = 0;
+
+    my @TodayDates = ( { 'Header' => "Heute - ".$Today->getDateStr } );
+    my @NextWeekDates = ( { 'Header' => "In den nächsten 7 Tagen" } );
+    my @OtherDates = ();
+
+    foreach my $DateObj ( $EventList->getDateList() )
+    {
+	my $OrgName = $DateObj->getOrg;
+
+	my $HashRef = { 'Time' => $DateObj->getTime,
+			'Place' => $DateObj->getPlace,
+			'Title' => $DateObj->getTitle,
+			'Desc' => $DateObj->getDesc }; #,
+#			'eMail' => $query->escapeHTML($OrgList{ $DateObj->getOrgID }->[1]),
+#			'Website' => $query->escapeHTML($OrgList{ $DateObj->getOrgID }->[2]) };
+
+	$HashRef->{'Org'} = $OrgName unless( $Intern );
+
+	if( $DateObj->getDate->getDateStr ne $LastDate )
+	{
+	    $HashRef->{'Date'} = $query->escapeHTML($DateObj->getDate->getDateStr);
+	    $LastDate = $DateObj->getDate->getDateStr;
+	}
+
+	if( $DateObj->getDate->isToday )
+	{
+	    push( @TodayDates, $HashRef );
+	}
+	elsif( $DateObj->getDate->isNextWeek )
+	{
+	    push( @NextWeekDates, $HashRef );
+	}
+	else
+	{
+	    if( $DateObj->getDate->getMonth != $LastMonth || $DateObj->getDate->getYear != $LastYear )
+	    {
+		my $TextMonth = $DateObj->getDate->getMonthText;
+		push( @OtherDates, { 'Header' => "$TextMonth ".$DateObj->getDate->getYear } );
+		
+		$LastMonth = $DateObj->getDate->getMonth;
+		$LastYear =  $DateObj->getDate->getYear;
+	    }
+
+	    push( @OtherDates, $HashRef );	
+	}
+    }
+
+    my @DateList = ();
+
+    push( @DateList, @TodayDates ) if @TodayDates > 1;
+    push( @DateList, @NextWeekDates ) if @NextWeekDates > 1;
+    push( @DateList, @OtherDates);
+
+    $SubTmpl->param('Dates' => \@DateList );
+
+    return $SubTmpl->output();
+}
+
+# -----------------------------------------------------------------------
+# Page Switch
+# Param-Hash: Org, Page, Pages, Internal
+# -----------------------------------------------------------------------
+sub PageSwitch(%)
+{
+    my %Param = @_;
+    my $Pages = $Param{Pages};
+    my $Page = $Param{Page};
+
+    my @Switch = ();
+    my %Result;
+    
+    my $Format = $Param{'ScriptURL'} || 'kalender.pl';
+
+    $Format .= ( index( $Format, '?' ) == -1 ) ? '?' : '&';
+    $Format .= "Seite=%d&Verein=%s";
+    $Format .= "&Intern=Intern" if $Param{Internal};
+    
+    for( my $i = 1; $i <= $Pages; $i++)
+    {
+	my $HashRef = { 'Page' => $i };
+	$HashRef->{'PageURL'} = sprintf($Format, $i, $Param{Org}) if $i != $Page;
+	$HashRef->{'IsCurrent'} = 1 if $i == $Page; 
+	
+	push( @Switch, $HashRef );
+    }
+
+    $Result{'Pages'} = \@Switch if $Pages > 1;
+    $Result{'NextPage'} = $Page + 1 if $Page < $Pages;
+    $Result{'NextPageURL'} = sprintf($Format, $Page + 1, $Param{Org}) if $Page < $Pages;
+    $Result{'PrevPage'} = $Page - 1 if $Page > 1;
+    $Result{'PrevPageURL'} = sprintf($Format, $Page - 1, $Param{Org}) if $Page > 1;
+    $Result{'PageCount'} = $Pages;
+    $Result{'CurrentPage'} = $Page;
+
+    return %Result;
+}
+
+1;
--- trunk/webeve/cgi-bin/kalender.pl
+++ trunk/webeve/cgi-bin/kalender.pl
@@ -8,6 +8,7 @@
 use strict;
 use CGI;
 use HTML::Template;
+use WebEve::View;
 use WebEve::Config;
 use WebEve::cEventList;
 use WebEve::cMySQL;
@@ -23,49 +24,9 @@ sub HTMLify($)
     return $string;
 }
 
-# -----------------------------------------------------------------------
-# Page Switch
-# Param-Hash: Org, Page, Pages, Internal
-# -----------------------------------------------------------------------
-sub PageSwitch(%)
-{
-    my %Param = @_;
-    my $Pages = $Param{Pages};
-    my $Page = $Param{Page};
-
-    my @Switch = ();
-    my %Result;
-    
-    my $Format = $Param{'ScriptURL'} || 'kalender.pl';
-
-    $Format .= ( index( $Format, '?' ) == -1 ) ? '?' : '&';
-    $Format .= "Seite=%d&Verein=%s";
-    $Format .= "&Intern=Intern" if $Param{Internal};
-    
-    for( my $i = 1; $i <= $Pages; $i++)
-    {
-	my $HashRef = { 'Page' => $i };
-	$HashRef->{'PageURL'} = sprintf($Format, $i, $Param{Org}) if $i != $Page;
-	$HashRef->{'IsCurrent'} = 1 if $i == $Page; 
-	
-	push( @Switch, $HashRef );
-    }
-
-    $Result{'Pages'} = \@Switch if $Pages > 1;
-    $Result{'NextPage'} = $Page + 1 if $Page < $Pages;
-    $Result{'NextPageURL'} = sprintf($Format, $Page + 1, $Param{Org}) if $Page < $Pages;
-    $Result{'PrevPage'} = $Page - 1 if $Page > 1;
-    $Result{'PrevPageURL'} = sprintf($Format, $Page - 1, $Param{Org}) if $Page > 1;
-    $Result{'PageCount'} = $Pages;
-    $Result{'CurrentPage'} = $Page;
-
-    return %Result;
-}
-
 sub main
 {
     my $MainTmplName = "kalender.tmpl";
-    my $SubTmplName = "kalender-table.tmpl";
     #WAP: "text/vnd.wap.wml"
 
     my $query = new CGI;
@@ -75,10 +36,9 @@ sub main
     my $Intern = $query->param('Intern') || 0; 
     $Intern = 0 if ! $Organization; 
 
-    my $Page = $query->param('Seite') || '1'; 
-
-    my $Embedded = $query->param('Embed') || '0';
-    my $Url = $Intern ? (getOrgPref($Organization, 'script-url-int'))[0] || '' : (getOrgPref($Organization, 'script-url'))[0] || '';
+    # my $Embedded = $query->param('Embed') || '0';
+    my $Url = $Intern ? (getOrgPref($Organization, 'script-url-int'))[0] || '' :
+	(getOrgPref($Organization, 'script-url'))[0] || '';
 
     # if( $Url && !$Embedded)
     # {
@@ -89,9 +49,6 @@ sub main
     my $tmp = sprintf("custom/template-%02d-simple.tmpl", $Organization);
     $MainTmplName = $tmp if( -f "$TemplatePath/$tmp" );
 
-    $tmp = sprintf("custom/template-%02d-advanced.tmpl", $Organization);
-    $SubTmplName = $tmp if( -f "$TemplatePath/$tmp" );
-
     if( lc($Template) eq 'druck')
     {
 	$MainTmplName = "print.tmpl";
@@ -100,93 +57,10 @@ sub main
     my $MainTmpl = HTML::Template->new(filename => "$TemplatePath/$MainTmplName",
 				       die_on_bad_params => 0);
 
-    my $SubTmpl = HTML::Template->new(filename => "$TemplatePath/$SubTmplName",
-				      die_on_bad_params => 0,
-				      loop_context_vars => 1);
-
-    # -----------------------------------------------------------------------
-
-    my $EventList = WebEve::cEventList->new( 'PerPage' => 15,
-					     'Page' => $Page,
-					     'PublicOnly' => !$Intern,
-					     'ForOrgID' => $Intern ? $Organization : 0 );
-    $EventList->readData();
-
-    $SubTmpl->param( PageSwitch( Org => $Organization,
-				 ScriptURL => $Url,
-				 Page => $Page,
-				 Pages => $EventList->getPageCount(),
-				 Internal => $Intern ) );
-    
-    # -----------------------------------------------------------------------
-
-    my $Today = WebEve::cDate->new('today');
-
-    # Terminliste
-    # -----------------------------------------------------------------------
-    my $LastDate = '';
-    my $LastMonth = 0;
-    my $LastYear = 0;
-    my $LastWasNextWeek = 0;
-
-    my @TodayDates = ( { 'Header' => "Heute - ".$Today->getDateStr } );
-    my @NextWeekDates = ( { 'Header' => "In den nächsten 7 Tagen" } );
-    my @OtherDates = ();
-
-    foreach my $DateObj ( $EventList->getDateList() )
-    {
-	my $OrgName = $DateObj->getOrg;
-
-	my $HashRef = { 'Time' => $DateObj->getTime,
-			'Place' => $DateObj->getPlace,
-			'Title' => $DateObj->getTitle,
-			'Desc' => $DateObj->getDesc }; #,
-#			'eMail' => $query->escapeHTML($OrgList{ $DateObj->getOrgID }->[1]),
-#			'Website' => $query->escapeHTML($OrgList{ $DateObj->getOrgID }->[2]) };
-
-	$HashRef->{'Org'} = $OrgName unless( $Intern );
-
-	if( $DateObj->getDate->getDateStr ne $LastDate )
-	{
-	    $HashRef->{'Date'} = $query->escapeHTML($DateObj->getDate->getDateStr);
-	    $LastDate = $DateObj->getDate->getDateStr;
-	}
-
-	if( $DateObj->getDate->isToday )
-	{
-	    push( @TodayDates, $HashRef );
-	}
-	elsif( $DateObj->getDate->isNextWeek )
-	{
-	    push( @NextWeekDates, $HashRef );
-	}
-	else
-	{
-	    if( $DateObj->getDate->getMonth != $LastMonth || $DateObj->getDate->getYear != $LastYear )
-	    {
-		my $TextMonth = $DateObj->getDate->getMonthText;
-		push( @OtherDates, { 'Header' => "$TextMonth ".$DateObj->getDate->getYear } );
-		
-		$LastMonth = $DateObj->getDate->getMonth;
-		$LastYear =  $DateObj->getDate->getYear;
-	    }
-
-	    push( @OtherDates, $HashRef );	
-	}
-    }
-
-    my @DateList = ();
-
-    push( @DateList, @TodayDates ) if @TodayDates > 1;
-    push( @DateList, @NextWeekDates ) if @NextWeekDates > 1;
-    push( @DateList, @OtherDates);
-
-    $SubTmpl->param('Dates' => \@DateList );
-
     $MainTmpl->param( 'Intern' => $Intern );
     $MainTmpl->param( 'Vereinsname' => getOrgName($Organization) ) if $Intern;
     $MainTmpl->param( 'VereinsID' => $Organization ) if $Intern;
-    $MainTmpl->param( 'Terminliste' => $SubTmpl->output );
+    $MainTmpl->param( 'Terminliste' => CalendarHTML($Url) );
 
     $MainTmpl->param('bgcolor' => getOrgPref($Organization, 'bgcolor'));
     $MainTmpl->param('bgimage' => getOrgPref($Organization, 'bgimage'));
